08774dfcf37d468f94d31348c4cbda93a6150d0b
aleperez5061 <aleperez5061@example.com>
2026-07-27 21:24:33 -0600

    LAs version

--- src/models/feedback.js
+++ src/models/feedback.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const FeedbackSchema = new mongoose.Schema({
     comentario: { type: String, required: true },
-    fecha: { type: Date, default: Date.now }
+    fecha: { type: Date, default: Date.now },
+    encuesta_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Encuesta', required: false }
 }, { collection: 'Feedback_Directo', versionKey: false });
 module.exports = mongoose.model('Feedback', FeedbackSchema);
